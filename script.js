5be76559c094193e0f32ed1ce8924313abd56ea3
OmVolodymyr94 <omelchenkowork94@gmail.com>
2021-09-19 17:51:25 +0300

    Formatted code

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-setInterval(function () {
+setInterval(() => {
 	const now = new Date();
 	const clock = document.getElementById("clock");
 	clock.innerHTML = now.toLocaleString();
 }, 1000);
+
 let backgroundImgSrc = "./img/background/Background.png";
-const changeBackground = event=>{
+const changeBackground = event => {
 	if (event.target.src){
 		backgroundImgSrc = event.target.src;
 		document.body.style.backgroundImage = `url(${backgroundImgSrc})`;
@@ -12,15 +13,15 @@ const changeBackground = event=>{
 		previoseImage.classList.remove("active");
 		event.target.classList.add("active");
 	}
-}
+};
 const images = document.getElementById("images");
-images.addEventListener("click",changeBackground);
+images.addEventListener("click", changeBackground);
 
 let isSettingsHide = false;
 const changeSettingsVision = () => {
 	isSettingsHide = !isSettingsHide;
 	const settingsModel = document.getElementById("settings");
 	settingsModel.style.display = isSettingsHide ? "none" : "block";
-}
+};
 const settingsIcon = document.querySelector(".settings-icon");
-settingsIcon.addEventListener("click",changeSettingsVision);
+settingsIcon.addEventListener("click", changeSettingsVision);
